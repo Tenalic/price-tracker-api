71fea4736771650ff7303083aa97a8267bc7c0d1
tenalic <stephane.cocquebert@gmail.com>
2026-07-03 18:13:51 +0200

    test fix redirection 2

--- src/main/java/com/pricetracker/controller/SpaController.java
+++ src/main/java/com/pricetracker/controller/SpaController.java
@@ -8,24 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 @Controller
 public class SpaController {
 
-    @GetMapping(value = {"/{path:^(?!api|actuator|assets).+$}", "/{path:^(?!api|actuator|assets).+$}/**"})
-    @ResponseBody
-    public String serveSpa() throws IOException {
-        // Serve index.html for all SPA routes (non-API, non-assets)
-        Resource indexHtml = new ClassPathResource("static/index.html");
-        return new String(Files.readAllBytes(indexHtml.getFile().toPath()), StandardCharsets.UTF_8);
-    }
-
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
-    public String serveIndex() throws IOException {
-        Resource indexHtml = new ClassPathResource("static/index.html");
-        return new String(Files.readAllBytes(indexHtml.getFile().toPath()), StandardCharsets.UTF_8);
+    public Resource serveIndex() throws IOException {
+        return new ClassPathResource("static/index.html");
+    }
+
+    @GetMapping(value = "/{path:^(?!api|actuator|assets).+$}", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public Resource serveSpaRoute() throws IOException {
+        return new ClassPathResource("static/index.html");
+    }
+
+    @GetMapping(value = "/{path:^(?!api|actuator|assets).+$}/**", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public Resource spaNestedRoute() throws IOException {
+        return new ClassPathResource("static/index.html");
     }
 }

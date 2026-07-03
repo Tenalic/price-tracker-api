ab06ec951ee6cc371e18de68097f1cd199e3ea22
tenalic <stephane.cocquebert@gmail.com>
2026-07-03 17:03:04 +0200

    test fix redirection

--- src/main/java/com/pricetracker/controller/SpaController.java
+++ src/main/java/com/pricetracker/controller/SpaController.java
@@ -1,17 +1,31 @@
 package com.pricetracker.controller;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 @Controller
 public class SpaController {
-    @GetMapping(value = "/{path:^(?!api).+$}")
-    public String forward() {
-        return "forward:/index.html";
+
+    @GetMapping(value = {"/{path:^(?!api|actuator|assets).+$}", "/{path:^(?!api|actuator|assets).+$}/**"})
+    @ResponseBody
+    public String serveSpa() throws IOException {
+        // Serve index.html for all SPA routes (non-API, non-assets)
+        Resource indexHtml = new ClassPathResource("static/index.html");
+        return new String(Files.readAllBytes(indexHtml.getFile().toPath()), StandardCharsets.UTF_8);
     }
 
-    @GetMapping(value = "/{path:^(?!api).+$}/{subpath:^(?!api).+$}")
-    public String forwardSubpath() {
-        return "forward:/index.html";
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String serveIndex() throws IOException {
+        Resource indexHtml = new ClassPathResource("static/index.html");
+        return new String(Files.readAllBytes(indexHtml.getFile().toPath()), StandardCharsets.UTF_8);
     }
 }

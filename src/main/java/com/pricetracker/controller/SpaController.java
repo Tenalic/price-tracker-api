21bca159fb210c7d78d4acd9c8cc42d710063002
tenalic <stephane.cocquebert@gmail.com>
2026-07-03 18:16:01 +0200

    simplification SpaController

--- src/main/java/com/pricetracker/controller/SpaController.java
+++ src/main/java/com/pricetracker/controller/SpaController.java
@@ -1,32 +1,14 @@
 package com.pricetracker.controller;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController {
 
-    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
-    @ResponseBody
-    public Resource serveIndex() throws IOException {
-        return new ClassPathResource("static/index.html");
+    @RequestMapping(value = {"/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+    public String redirect() {
+        return "forward:/index.html";
     }
 
-    @GetMapping(value = "/{path:^(?!api|actuator|assets).+$}", produces = MediaType.TEXT_HTML_VALUE)
-    @ResponseBody
-    public Resource serveSpaRoute() throws IOException {
-        return new ClassPathResource("static/index.html");
-    }
-
-    @GetMapping(value = "/{path:^(?!api|actuator|assets).+$}/**", produces = MediaType.TEXT_HTML_VALUE)
-    @ResponseBody
-    public Resource spaNestedRoute() throws IOException {
-        return new ClassPathResource("static/index.html");
-    }
 }
